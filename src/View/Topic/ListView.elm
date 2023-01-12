be2d4b930e69c1c7712cae7572afe186ab42c2a4
flytreeleft <flytreeleft@crazydan.org>
2023-01-12 17:08:12 +0800

    Frontend: 使用Element.Keyed与Element.Lazy渲染Topic列表，以提升列表的性能

--- src/View/Topic/ListView.elm
+++ src/View/Topic/ListView.elm
@@ -1,6 +1,8 @@
 module View.Topic.ListView exposing (create)
 
 import Element exposing (..)
+import Element.Keyed
+import Element.Lazy
 import Model.Root exposing (RootModel)
 import Model.Topic
 import Msg exposing (RootMsg)
@@ -12,7 +14,7 @@ import Widget.Markdown
 
 create : RootModel -> Element RootMsg
 create model =
-    topicList model.topics
+    topicListView model.topics
 
 
 
@@ -32,35 +34,46 @@ getPaletteWithDefault bgColor defaultPalette =
             defaultPalette
 
 
-topicList : List Model.Topic.Topic -> Element RootMsg
-topicList topics =
-    column
+topicListView : List Model.Topic.Topic -> Element RootMsg
+topicListView topics =
+    -- 列表增删改性能提升方案，同时lazy可确保在刷新页面时，有滚动条的列表的滚动位置可被浏览器记录，刷新后能够自动恢复浏览位置
+    -- https://guide.elm-lang.org/optimization/keyed.html
+    -- https://package.elm-lang.org/packages/mdgriffith/elm-ui/latest/Element-Keyed
+    -- https://package.elm-lang.org/packages/mdgriffith/elm-ui/latest/Element-Lazy
+    Element.Keyed.column
         Style.Topic.topicList
         (topics
             |> List.map
                 (\topic ->
-                    column
-                        Style.Topic.topic
-                        [ row
-                            (Style.Topic.topicBody
-                                ++ getPaletteWithDefault topic.color
-                                    Style.Topic.topicBodyDefaultPalette
-                            )
-                            [ el
-                                [ width fill
-                                , padding 8
-                                ]
-                                (el
-                                    Style.Topic.contentInTopicBody
-                                    (paragraph
-                                        [ height shrink
-                                        , centerY
-                                        ]
-                                        [ Widget.Markdown.render topic.content
-                                        ]
-                                    )
-                                )
-                            ]
-                        ]
+                    ( topic.id
+                    , Element.Lazy.lazy topicView topic
+                    )
                 )
         )
+
+
+topicView : Model.Topic.Topic -> Element RootMsg
+topicView topic =
+    column
+        Style.Topic.topic
+        [ row
+            (Style.Topic.topicBody
+                ++ getPaletteWithDefault topic.color
+                    Style.Topic.topicBodyDefaultPalette
+            )
+            [ el
+                [ width fill
+                , padding 8
+                ]
+                (el
+                    Style.Topic.contentInTopicBody
+                    (paragraph
+                        [ height shrink
+                        , centerY
+                        ]
+                        [ Widget.Markdown.render topic.content
+                        ]
+                    )
+                )
+            ]
+        ]

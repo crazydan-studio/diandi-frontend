eea1f49e3ede698238aefc1d6fb3540970d99377
flytreeleft <flytreeleft@crazydan.org>
2023-01-29 10:36:03 +0800

    优化clickOutOfMe事件触发性能

--- public/js/event.js
+++ public/js/event.js
@@ -1,22 +1,25 @@
 "use strict";
 
 let customEventNextId = 0;
-const eventIdAttrName = "event-id";
-const eventTypeClickOutOfMe = "clickOutOfMe";
+const EVENT_ID_ATTR_NAME = "event-id";
+const EVENT_TYPE_CLICK_OUT_OF_ME = "clickOutOfMe";
 
 const customEvents = {
-  [eventTypeClickOutOfMe]: [],
+  [EVENT_TYPE_CLICK_OUT_OF_ME]: [],
 };
 
 const oldAddEventListener = Node.prototype.addEventListener;
 Node.prototype.addEventListener = function (type) {
   const el = this;
 
-  if (customEvents.hasOwnProperty(type) && !el.getAttribute(eventIdAttrName)) {
-    const eventId = customEventNextId++;
+  if (
+    customEvents.hasOwnProperty(type) &&
+    !el.getAttribute(EVENT_ID_ATTR_NAME)
+  ) {
+    const eventId = customEventNextId++ + "";
 
     customEvents[type].push(eventId);
-    el.setAttribute(eventIdAttrName, eventId);
+    el.setAttribute(EVENT_ID_ATTR_NAME, eventId);
   }
 
   oldAddEventListener.apply(el, arguments);
@@ -26,18 +29,29 @@ Node.prototype.addEventListener = function (type) {
 document.addEventListener("click", function (event) {
   let el = event.target;
 
-  customEvents[eventTypeClickOutOfMe].forEach(function (eventId) {
-    const selector = `[${eventIdAttrName}="${eventId}"]`;
-
-    while (el) {
-      if (el.getAttribute(eventIdAttrName) === eventId + "") {
-        return;
-      }
-      el = el.parentElement;
+  const parentEventIds = [];
+  while (el && el !== document.body) {
+    const eventId = el.getAttribute(EVENT_ID_ATTR_NAME);
+    if (eventId) {
+      parentEventIds.push(eventId);
     }
 
+    el = el.parentElement;
+  }
+
+  const registerEventIds = customEvents[EVENT_TYPE_CLICK_OUT_OF_ME];
+  const triggerEventIds =
+    parentEventIds.length === 0
+      ? registerEventIds
+      : registerEventIds.filter(function (eventId) {
+          return !parentEventIds.includes(eventId);
+        });
+
+  triggerEventIds.forEach(function (eventId) {
+    const selector = `[${EVENT_ID_ATTR_NAME}="${eventId}"]`;
+
     document.querySelectorAll(selector).forEach(function (node) {
-      node.dispatchEvent(new Event(eventTypeClickOutOfMe));
+      node.dispatchEvent(new Event(EVENT_TYPE_CLICK_OUT_OF_ME));
     });
   });
 });
